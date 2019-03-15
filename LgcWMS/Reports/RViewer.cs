9c82d32177240c473c95a411fafd9f89b5bc34c9
A. Mauricio Torres Silva <teldot@gmail.com>
2019-03-15 13:08:27 -0500

    Type: Fea
    -Label fixed

--- LgcWMS/Reports/RViewer.cs
+++ LgcWMS/Reports/RViewer.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LgcWMS.Business.PrintDocs;
 
 namespace LgcWMS.Reports
 {
@@ -60,8 +61,8 @@ namespace LgcWMS.Reports
 
                     pSett = new PrinterSettings();
                     pSett.Copies = 1;
-                    pSett.FromPage = 1;
-                    pSett.ToPage = 1;
+                    //pSett.FromPage = 1;
+                    //pSett.ToPage = 1;
                     pSett.PrinterName = PrinterName;
                     rv.PrintDialog(pSett);
 
@@ -71,37 +72,44 @@ namespace LgcWMS.Reports
                 #endregion
                 case ActionType.Label:
                     #region Label
-                    pg = new PageSettings();
-                    pg.Margins.Top = 0;
-                    pg.Margins.Bottom = 0;
-                    pg.Margins.Left = 5;
-                    pg.Margins.Right = 5;
-                    size = new PaperSize();
-                    size.RawKind = (int)PaperKind.Custom;
-                    size.Height = 480;
-                    size.Width = 400;
-                    pg.PaperSize = size;
-                    pg.Landscape = false;
-                    this.GUIA_DUOBindingSource.DataSource = D_Source;//typeof(LgcWMS.Data.Model.V_GUIA);
-                    reportDataSource1.Name = "DataSetGuiaDuo";
-                    reportDataSource1.Value = this.GUIA_DUOBindingSource;
-                    //ReportDataSource rpSource = new ReportDataSource("GuiaDuo", D_Source);
-                    //this.V_GUIABindingSource.DataSource = rpSource;
-                    this.rv.LocalReport.DataSources.Add(reportDataSource1);
-                    this.rv.LocalReport.ReportEmbeddedResource = "LgcWMS.Reports.LblDespacho.rdlc";
-                    rv.SetPageSettings(pg);
-                    this.rv.RefreshReport();
-                    rv.SetDisplayMode(DisplayMode.PrintLayout);
+                    //    pg = new PageSettings();
+                    //    pg.Margins.Top = 0;
+                    //    pg.Margins.Bottom = 0;
+                    //    pg.Margins.Left = 5;
+                    //    pg.Margins.Right = 5;
+                    //    size = new PaperSize();
+                    //    size.RawKind = (int)PaperKind.Custom;
+                    //    size.Height = 480;
+                    //    size.Width = 400;
+                    //    pg.PaperSize = size;
+                    //    pg.Landscape = false;
+                    //    this.GUIA_DUOBindingSource.DataSource = D_Source;//typeof(LgcWMS.Data.Model.V_GUIA);
+                    //    reportDataSource1.Name = "DataSetGuiaDuo";
+                    //    reportDataSource1.Value = this.GUIA_DUOBindingSource;
+                    //    //ReportDataSource rpSource = new ReportDataSource("GuiaDuo", D_Source);
+                    //    //this.V_GUIABindingSource.DataSource = rpSource;
+                    //    this.rv.LocalReport.DataSources.Clear();
+                    //    this.rv.LocalReport.DataSources.Add(reportDataSource1);
+                    //    this.rv.LocalReport.ReportEmbeddedResource = "LgcWMS.Reports.LblDespacho.rdlc";
+                    //    rv.SetPageSettings(pg);
+                    //    /*rv.RefreshReport();*/
+                    //    rv.SetDisplayMode(DisplayMode.PrintLayout);
 
-                    //pSett = new PrinterSettings();
-                    //pSett.Copies = 1;
-                    //pSett.FromPage = 1;
-                    //pSett.ToPage = 1;
-                    //pSett.PrinterName = PrinterName;
-                    //rv.PrintDialog(pSett);
-                    rv.PrintDialog();
+                    //    //pSett = new PrinterSettings();
+                    //    //pSett.Copies = 1;
+                    //    //pSett.FromPage = 1;
+                    //    //pSett.ToPage = 1;
+                    //    //pSett.PrinterName = PrinterName;
+                    //    //rv.PrintDialog(pSett);
+                    //    rv.RefreshReport();
+                    //    rv.PrintDialog();
+                    //    break;
+                    #endregion
+
+                    LabelPrintDoc printDoc = new LabelPrintDoc(400, 480);
+                    printDoc.Images.Add(Properties.Resources.logoblanconegro);
+                    printDoc.PrintDoc();
                     break;
-                #endregion
                 default:
                     break;
             }
